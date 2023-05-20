0bea361dc6a644227f38a4587b4427962e78f573
Dolphin Oracle <dolphinoracle@gmail.com>
2023-05-19 20:29:25 -0400

    mo files

--- translations_script/update-po-mo.sh
+++ translations_script/update-po-mo.sh
@@ -26,13 +26,12 @@ fi
 xgettext --language Shell  --add-comments --sort-output -o pot/$RESOURCE.pot ../mxfb-menu-generator ../mxfb-tint2-manager ../mxfb-trashcheck ../mxfb-bg-rotate ../mxfb-borders ../mxfb-font ../mxfb-quickshot ../mxfb-separate-backgrounds ../mxfb-tiles ../mxfb-timer ../mxfb-top ../mxfb-look ../exit-options-manager
 }
 
-##during deb package build
 make_mo()
 {
     for val in $lang; do
-        if [ ! -e "translations_script/mo/$val/$RESOURCE.mo" ]; then
-            mkdir -p translations_script/mo/$val
-            msgfmt --output-file=translations_script/mo/$val/"$RESOURCE".mo translations_script/po/"${RESOURCE}_${val}.po"
+        if [ ! -e "mo/$val/$RESOURCE.mo" ]; then
+            mkdir -p mo/$val
+            msgfmt --output-file=mo/$val/"$RESOURCE".mo po/"${RESOURCE}_${val}.po"
         fi
     done
 }
